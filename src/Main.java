07f93ac27a4edf285095d3649ab2063f6d9597fa
Yevheniy_Podplietnii <magisckyevgeshka@gmail.com>
2021-03-20 10:40:39 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String firstName ;
+        String surname ;
+        String patronymic ;
+        int age;
+        int birthYear;
+
+        System.out.print("Enter your first name: ");
+        firstName = scan.next();
+        System.out.print("Enter your surname: ");
+        surname = scan.next();
+        System.out.print("Enter your patronymic: ");
+        patronymic = scan.next();
+        System.out.print("Enter your age: ");
+        age = scan.nextInt();
+
+        birthYear = 2021 - age;
+
+        System.out.printf("Hello, %s %s %s ,who was born in %d year.", firstName, surname, patronymic, birthYear);
+
+    }
+}
